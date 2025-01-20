10a729e590493afe02a5be2d2e8f7fc8021a1328
ericafenyo <ericafenyo@yahoo.com>
2025-01-20 13:05:15 +0100

    feat: (#10) add login component with auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { MissionsComponent } from './missions/missions.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'missions',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
     path: 'missions',
     component: MissionsComponent,
+    canActivate: [AuthGuard],
   },
 ];
