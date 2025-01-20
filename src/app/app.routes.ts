116f60db61f4ddafc5eafc204b2533e407a9e374
ericafenyo <ericafenyo@yahoo.com>
2025-01-20 12:11:10 +0100

    feat: (#10) add bare missions components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MissionsComponent } from './missions/missions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'missions',
+    component: MissionsComponent,
+  },
+];
