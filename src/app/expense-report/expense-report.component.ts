07bd88a44915bcdf3ec987c01a260a920bc611d2
Marjory PRIN <marjory.prin@gmail.com>
2025-01-22 16:26:24 +0100

    (WIP) feat: add dynamic expense report loading by ID

--- src/app/expense-report/expense-report.component.ts
+++ src/app/expense-report/expense-report.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MissionSummaryComponent} from './mission-summary/mission-summary.component';
 import {ExpenseTableComponent} from './expense-table/expense-table.component';
@@ -6,7 +6,8 @@ import {Status} from "../interfaces/status";
 import {Mission} from "../interfaces/mission";
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from "@angular/material/button";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ExpenseService} from '@/app/services/expense.service';
 
 @Component({
   selector: 'app-expense-report',
@@ -37,6 +38,44 @@ export class ExpenseReportComponent implements OnInit {
     private router: Router,
     public dialog: MatDialog
   ) {}
+
+  ngOnInit(): void {
+    this.loadExpenseReport()
+    //TODO: à supprimer après verif quand authent effective
+    this.expenseService.getExpensesByExpenseReportId(123).subscribe(expenses => {
+      console.log('Données récupérées par le service des dépenses :', expenses);
+    });
+  }
+
+  //TODO: à refaire sans le mock quand Mission service sera près
+  loadExpenseReport() {
+    const expenseReportId = Number(this.route.snapshot.paramMap.get('id'));
+    if (!isNaN(expenseReportId)) {
+      this.expenseService.getExpensesByExpenseReportId(expenseReportId).subscribe({
+        next: (expenses) => {
+          const mockMission: Mission = {
+            id: 0,
+            startDate: new Date(),
+            endDate: new Date(),
+            startTown: 'Placeholder',
+            endTown: 'Placeholder',
+            status: Status.INITIALE,
+            transportIds: [],
+            expenseReport: {
+              id: expenseReportId,
+              amount: expenses.reduce((sum, e) => sum + e.amount, 0),
+              status: Status.INITIALE,
+              expenses: expenses
+            }
+          };
+          this.mission.set(mockMission);
+        },
+        error: (error) => {
+          console.error('Erreur lors de la récupération de la note de frais: ', error);
+        }
+      })
+    }
+  }
 
   ngOnInit(): void {
     this.loadExpenseReport()
