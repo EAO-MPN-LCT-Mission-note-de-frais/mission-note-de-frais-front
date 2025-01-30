346878c1e1dc9cfffa7c3cffab547de4d1c4f231
Marjory PRIN <marjory.prin@gmail.com>
2025-01-30 14:58:17 +0100

    feat: update login with error handling and password visibility

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {
   FormControl,
   FormsModule,
@@ -9,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { AuthService } from '../services/auth.service';
+import {ErrorHandlerService} from '@/app/utils/error-handler.service';
 
 @Component({
   selector: 'app-login',
@@ -23,16 +24,24 @@ import { AuthService } from '../services/auth.service';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+
+  private authService = inject(AuthService);
+  private errorHandlerService = inject(ErrorHandlerService);
+
   email = new FormControl('', Validators.required);
   password = new FormControl('', Validators.required);
 
-  constructor(private authService: AuthService) {}
-
   onSubmit(event: SubmitEvent) {
     event.preventDefault();
-    console.log(this.email.value, this.password.value);
     const email = this.email.value ?? '';
     const password = this.password.value ?? '';
-    this.authService.authenticate(email, password);
+    this.authService.authenticate(email, password).subscribe({
+      next: () => {
+        console.log('Authentifier avec succès.');
+      },
+      error: (error) => {
+        this.errorHandlerService.handleError(error, "Le mot de passe est ou l'email est inconnu.");
+      }
+    });
   }
 }
